8d168cf45af8378ff42f978682f868296595c528
Rouven Czerwinski <rouven@czerwinskis.de>
2022-10-07 13:50:41 +0200

    main: run cargo fmt
    Signed-off-by: Rouven Czerwinski <rouven@czerwinskis.de>

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,10 @@ enum Commands {
     Download,
 }
 
-use anyhow::{Result, Context, anyhow};
+use anyhow::{anyhow, Context, Result};
 
-use serde::Deserialize;
 use notify_rust::Notification;
+use serde::Deserialize;
 
 #[derive(Debug)]
 struct Secrets {
@@ -68,7 +68,10 @@ async fn prompt_user_password() -> Result<(String, String)> {
     std::io::stdout().flush()?;
     let mut line = String::new();
     std::io::stdin().read_line(&mut line)?;
-    line = line.strip_suffix('\n').context("Couldn't strip newline from input, no input?")?.to_string();
+    line = line
+        .strip_suffix('\n')
+        .context("Couldn't strip newline from input, no input?")?
+        .to_string();
     let password = prompt_password("Password: ")?;
     Ok((line, password))
 }
@@ -93,16 +96,17 @@ impl AppState<'_> {
                 print!("2FA Token:");
                 std::io::stdout().flush()?;
                 std::io::stdin().read_line(&mut token)?;
-                token = token.strip_suffix('\n').context("Couldn't strip newline from input, no input?")?.to_string();
+                token = token
+                    .strip_suffix('\n')
+                    .context("Couldn't strip newline from input, no input?")?
+                    .to_string();
                 params.insert("twofa", token);
                 let res = client.post(login_url).form(&params).send().await?;
                 let status = res.status();
                 let json: POOCAPIResponse = res.json().await?;
                 match status {
-                    reqwest::StatusCode::OK => {
-                        Ok(json.secret.unwrap())
-                    }
-                    _ => Err(anyhow!("Unhandled status code from Open Client API"))
+                    reqwest::StatusCode::OK => Ok(json.secret.unwrap()),
+                    _ => Err(anyhow!("Unhandled status code from Open Client API")),
                 }
             }
             reqwest::StatusCode::OK => {
@@ -111,7 +115,10 @@ impl AppState<'_> {
             }
             _ => {
                 assert!(json.status == 0);
-                Err(anyhow!("Unhandled status code from Open Client API: {:?}", json))
+                Err(anyhow!(
+                    "Unhandled status code from Open Client API: {:?}",
+                    json
+                ))
             }
         }
     }
@@ -127,7 +134,10 @@ impl AppState<'_> {
         println!("Device name:");
         let mut name = String::new();
         std::io::stdin().read_line(&mut name)?;
-        name = name.strip_suffix('\n').context("Couldn't strip newline from input, no input?")?.to_string();
+        name = name
+            .strip_suffix('\n')
+            .context("Couldn't strip newline from input, no input?")?
+            .to_string();
 
         params.insert("name", name);
 
@@ -143,11 +153,13 @@ impl AppState<'_> {
             }
             _ => {
                 assert!(json.status == 0);
-                Err(anyhow!("Unhandled status code from Open Client API: {:?}", json))
+                Err(anyhow!(
+                    "Unhandled status code from Open Client API: {:?}",
+                    json
+                ))
             }
         }
     }
-
 
     async fn download_messages(&self) -> Result<Option<Vec<POMessage>>> {
         let download_url = "https://api.pushover.net/1/messages.json";
@@ -160,7 +172,9 @@ impl AppState<'_> {
         println!("Response: {:?}", res);
         let json: POOCAPIResponse = res.json().await?;
         assert!(json.status == 1);
-        let messages = json.messages.context("Messages Key not found in API response")?;
+        let messages = json
+            .messages
+            .context("Messages Key not found in API response")?;
         match messages.len() {
             0 => Ok(None),
             _ => Ok(Some(messages)),
@@ -168,12 +182,19 @@ impl AppState<'_> {
     }
 
     async fn delete_messages(&self, messages: &[POMessage]) -> Result<()> {
-        let device_id = &self.secrets.context("Could not retrieve secrets from storage")?.device_id;
-        let delete_url = format!("https://api.pushover.net/1/devices/{device_id}/update_highest_message.json");
+        let device_id = &self
+            .secrets
+            .context("Could not retrieve secrets from storage")?
+            .device_id;
+        let delete_url =
+            format!("https://api.pushover.net/1/devices/{device_id}/update_highest_message.json");
         let mut params = HashMap::new();
         let client = &self.client;
         let secrets = &self.secrets.context("Could not load secret from storage")?;
-        let max = messages.iter().fold(0, |max, x| if x.id > max { x.id } else { max }).to_string();
+        let max = messages
+            .iter()
+            .fold(0, |max, x| if x.id > max { x.id } else { max })
+            .to_string();
 
         params.insert("secret", &secrets.secret);
         params.insert("message", &max);
@@ -187,20 +208,19 @@ impl AppState<'_> {
 }
 
 impl Secrets {
-
     async fn get_secret_available(&self, tos: &str) -> Result<Option<String>> {
         let mut attributes = HashMap::new();
         attributes.insert("application", "tsuna");
         attributes.insert("type", tos);
 
-        let items = self.keyring
-            .search_items(attributes)
-            .await?;
+        let items = self.keyring.search_items(attributes).await?;
 
         if items.is_empty() {
             return Ok(None);
         }
-        Ok(Some(std::str::from_utf8(items[0].secret().await?.as_slice())?.to_string()))
+        Ok(Some(
+            std::str::from_utf8(items[0].secret().await?.as_slice())?.to_string(),
+        ))
     }
 
     async fn store_secrets(&self) -> Result<()> {
@@ -208,11 +228,20 @@ impl Secrets {
         attributes.insert("application", "tsuna");
         attributes.insert("type", "secret");
 
-        self.keyring.create_item("Tsuna secret", attributes, self.secret.as_bytes(), true).await?;
+        self.keyring
+            .create_item("Tsuna secret", attributes, self.secret.as_bytes(), true)
+            .await?;
         let mut attributes = HashMap::new();
         attributes.insert("application", "tsuna");
         attributes.insert("type", "device_id");
-        self.keyring.create_item("Tsuna device_id", attributes, self.device_id.as_bytes(), true).await?;
+        self.keyring
+            .create_item(
+                "Tsuna device_id",
+                attributes,
+                self.device_id.as_bytes(),
+                true,
+            )
+            .await?;
 
         Ok(())
     }
@@ -243,27 +272,28 @@ impl Secrets {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
     let args = Cli::parse();
 
     let mut secrets = Secrets::new().await?;
 
     let mut state = AppState {
-        client: reqwest::Client::builder().user_agent(APP_USER_AGENT).build()?,
+        client: reqwest::Client::builder()
+            .user_agent(APP_USER_AGENT)
+            .build()?,
         secrets: None,
     };
 
     if args.command == Commands::Register {
-            let sec_opt = secrets.get_secret_available("secret").await?;
-            let dev_opt = secrets.get_secret_available("device_id").await?;
-            if sec_opt != None || dev_opt != None {
-                print!("Device already registered, please explictly delete with 'delete'");
-                return Ok(());
-            }
-            secrets.secret = state.login().await?;
-            secrets.device_id = state.register_device(&secrets.secret).await?;
-            secrets.store_secrets().await?;
+        let sec_opt = secrets.get_secret_available("secret").await?;
+        let dev_opt = secrets.get_secret_available("device_id").await?;
+        if sec_opt != None || dev_opt != None {
+            print!("Device already registered, please explictly delete with 'delete'");
             return Ok(());
+        }
+        secrets.secret = state.login().await?;
+        secrets.device_id = state.register_device(&secrets.secret).await?;
+        secrets.store_secrets().await?;
+        return Ok(());
     }
 
     let sec_opt = secrets.get_secret_available("secret").await?;
@@ -279,33 +309,28 @@ async fn main() -> Result<()> {
     state.secrets = Some(&secrets);
 
     match args.command {
-        Commands::Delete => {
-            secrets.delete_secrets().await
-        },
+        Commands::Delete => secrets.delete_secrets().await,
         Commands::Download => {
-
             let messages = state.download_messages().await?;
 
             match messages {
                 Some(ref m) => {
                     for message in m {
-                        Notification::new().summary(&message.title)
-                                           .body(&message.message).show()?;
+                        Notification::new()
+                            .summary(&message.title)
+                            .body(&message.message)
+                            .show()?;
                     }
-                },
-                None => {},
+                }
+                None => {}
             }
 
             if let Some(m) = &messages {
                 state.delete_messages(m).await?;
             }
 
-
             Ok(())
-        },
+        }
         Commands::Register => Ok(()),
-
     }
-
-
 }
